799555b7aaa7e27a513fe472eb9f04d8b39a76a4
kuznetsovandrey76 <and.rey.q@yandex.ru>
2019-02-17 22:03:07 +0300

    need fix button join and corner buttons

--- build/js/main.js
+++ build/js/main.js
@@ -1,1 +1,1 @@
-"use strict";$(document).ready(function(){});
+"use strict";$(document).ready(function(){$(".footer-menu__heading").on("click",function(){$(".corner-up").toggleClass("corner-up corner-down"),$(this).toggleClass("corner-down"),$(this).toggleClass("corner-up")})});
--- src/js/scripts/custom.js
+++ src/js/scripts/custom.js
@@ -1,3 +1,7 @@
 $( document ).ready(function() {
-
-});
+    $('.footer-menu__heading').on('click', function() {
+        $('.corner-up').toggleClass("corner-up corner-down");
+        $(this).toggleClass("corner-down");
+        $(this).toggleClass("corner-up");
+    });
+}); 
